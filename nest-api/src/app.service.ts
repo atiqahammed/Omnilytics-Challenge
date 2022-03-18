9354bdb02e76848661d44a9ceef526b9c6c66aed
Atiq Ahammed <atiqahammedshamim@gmail.com>
2022-03-18 19:53:40 +0600

    generate file initial end point created

--- nest-api/src/app.service.ts
+++ nest-api/src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { GenerateFileResponse } from './dto/generate.file.response.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  generateFile(): GenerateFileResponse {
+    let response = new GenerateFileResponse();
+    response.isSuccess = true;
+    response.fileName = 'RandomFile.txt';
+
+    return response;
   }
 }
---
+++ nest-api/src/dto/generate.file.response.dto.ts
@@ -0,0 +1,4 @@
+export class GenerateFileResponse {
+    isSuccess: boolean;
+    fileName: string;
+}
